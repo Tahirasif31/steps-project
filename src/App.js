df3fa7e445447134d66d39ebd42538a20c8db366
tahirasi31 <tahirasif31@gmail.com>
2023-10-20 22:59:36 +0500

    step buttons done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="steps">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        <div>
+          <h2>Step: 1</h2>
+        </div>
+        p
+      </div>
+    </div>
+  );
+}
+
+export default App;
